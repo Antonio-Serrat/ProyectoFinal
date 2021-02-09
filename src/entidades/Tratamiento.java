5477dd712013c9b67c655289b34dce07e7ae8089
tonisrt <tonisrt@ToniSrt>
2021-02-09 02:15:50 -0300

    .

--- src/entidades/Tratamiento.java
+++ src/entidades/Tratamiento.java
@@ -5,7 +5,7 @@ import java.util.Date;
 public class Tratamiento extends Pacientes {
 
 	private Date Fecha;
-	private String Prescripción;
+	private String Prescripcion;
 	private String Evolucion;
 	private String Observaciones;
 	
